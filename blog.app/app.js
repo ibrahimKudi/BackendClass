435a03347a63762aff283a132e0e9fca9c499d29
Kudi <ibrahimabdulquddus31@gmail.com>
2025-03-25 12:19:38 +0100

    day2on-blogapp

--- blog.app/app.js
+++ blog.app/app.js
@@ -1,14 +1,19 @@
 require('dotenv').config()
-const express = require('express')
-const app = express()
+const express = require('express');
+const expressLayout = require("express-ejs-layouts")
+const app = express();
 
 PORT = 4000 || process.env.PORT
 
+app.use(express.static('./public'))
+
+//Template Engine
+app.use(expressLayout)
+app.set('layout', 'layouts/main')
+app.set('view engine', 'ejs')
+
 app.use('/', require('./routes/main'));
 
-// app.get('/', (req, res) =>{
-//     res.send('Hello World!')
-// })
 
 app.listen(PORT, () => {
     console.log(`App listening on port ${PORT}`);
--- blog.app/routes/main.js
+++ blog.app/routes/main.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const router = express.Router();
 
-router.get('', (req, res) => {
-    res.send('hello KuDi')
+router.get("", (req, res) => {
+    const locals = {
+        title: "Nodejs Blog App",
+    description: "A Nodejs Blog App built with Nodejs, Express and mongoDb"
+    }
+    res.render('index', {locals})
 });
 
 module.exports = router
